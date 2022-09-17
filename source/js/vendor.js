4d0abc1f9491231b735e1e33a5a0e93b664eb275
kmartin-ru <i@kmartin.ru>
2022-09-17 12:56:53 +0300

    added animated noise for background

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,2 +1,11 @@
-// Swiper 7.4.1
-// import './vendor/swiper';
+import './vendor/grained';
+
+grained('#noise', {
+  'animate': true,
+  'patternWidth': 500,
+  'patternHeight': 500,
+  'grainOpacity': 0.05,
+  'grainDensity': 1,
+  'grainWidth': 1.15,
+  'grainHeight': 1.15,
+});
