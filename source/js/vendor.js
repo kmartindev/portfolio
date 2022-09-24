4096ca8470553dfb472f9fe35aeec26eef9998f9
kmartin-ru <i@kmartin.ru>
2022-09-24 06:36:05 +0300

    added links

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -4,7 +4,7 @@ grained('#noise', {
   'animate': true,
   'patternWidth': 500,
   'patternHeight': 500,
-  'grainOpacity': 0.1,
+  'grainOpacity': 0.075,
   'grainDensity': 2,
   'grainWidth': 1,
   'grainHeight': 1,
