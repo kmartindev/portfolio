e77e8a02cbeedee42d2200516c0e7904ad5c85d4
kmartin-ru <i@kmartin.ru>
2022-09-24 04:57:27 +0300

    added favicons

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -4,8 +4,8 @@ grained('#noise', {
   'animate': true,
   'patternWidth': 500,
   'patternHeight': 500,
-  'grainOpacity': 0.05,
-  'grainDensity': 1,
-  'grainWidth': 1.15,
-  'grainHeight': 1.15,
+  'grainOpacity': 0.1,
+  'grainDensity': 2,
+  'grainWidth': 1,
+  'grainHeight': 1,
 });
